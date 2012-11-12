c1de66e8b72625c7fd848373b0ace20034b1653c
Irio Irineu Musskopf Junior <irio.musskopf@caixadeideias.com.br>
2012-11-12 10:18:27 -0200

    Initializing project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Captar::Application.config.secret_token = '5c0e76b3c3d279db53e63ada40a0493946cb39844a85a6b151c0b7d0e5d103e268453bc993a9bf483e0d0ecb3c961e15394ed9e1e7d3e2d031e21f776a584991'
