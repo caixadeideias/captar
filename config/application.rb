1d5d1eac050cc8de44d5a062c9d322aa478a909f
Irio Irineu Musskopf Junior <irio.musskopf@caixadeideias.com.br>
2012-12-03 21:34:21 -0200

    Set config.assets.initialize_on_precompile option to false

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,9 @@ end
 
 module Captar
   class Application < Rails::Application
+    # Heroku
+    config.assets.initialize_on_precompile = false
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
